36362b4380c50e2dd5131ea1f658fd236129984f
IP Susila <susila.ip@gmail.com>
2017-07-03 19:29:15 +0700

    Initial commit

---
+++ http/formuploader.go
@@ -0,0 +1,332 @@
+package http
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+//FormUploader encapsulates HTTP multipart form.
+type FormUploader interface {
+	AddField(name, value string) FormUploader
+	AddFields(fields map[string]string) FormUploader
+	Fields(name string) []string
+	AddFiles(fieldName string, filesPath ...string) error
+	Files() []string
+	SetChunkSize(size int64) FormUploader
+	ChunkSize() int64
+	Post(targetURL string) (*http.Response, error)
+	Put(targetURL string) (*http.Response, error)
+}
+
+type formPart interface {
+	newPart(buf *bytes.Buffer, mpw *multipart.Writer) (int64, error)
+	writeTo(chunk []byte, w io.Writer) error
+	close() error
+}
+
+type fieldPart struct {
+	name   string //field name
+	value  string //field value
+	mpData []byte //multipart data
+}
+
+type filePart struct {
+	file      *os.File //File handler
+	fileSize  int64    //File size
+	baseName  string   //Base name (may differs from orignal base-name)
+	filePath  string   //Original filename
+	fieldName string   //Name of field in multipart content
+	mpBegin   []byte   //Beginning of the multipart
+}
+
+type endPart struct {
+	mpData []byte //multipart data
+}
+
+type formUploader struct {
+	chunkSize int64
+	fields    []*fieldPart
+	files     []*filePart
+}
+
+//NewFormUploader creates form uploder instance.
+func NewFormUploader() FormUploader {
+	return &formUploader{chunkSize: 1024 * 10}
+}
+
+//write all the data to writer
+func writeExactly(w io.Writer, data []byte) error {
+	if ndata := len(data); ndata > 0 {
+		nw := 0
+		for nw < ndata {
+			n, err := w.Write(data[nw:])
+			if err != nil {
+				return err
+			}
+			nw += n
+		}
+	}
+	return nil
+}
+
+func (p *fieldPart) newPart(buf *bytes.Buffer, mpw *multipart.Writer) (int64, error) {
+	//Create multipart data
+	if err := mpw.WriteField(p.name, p.value); err != nil {
+		return 0, err
+	}
+
+	//Read writen data from buffer
+	n := buf.Len()
+	if cap(p.mpData) < n {
+		p.mpData = make([]byte, n)
+	}
+	nr, err := buf.Read(p.mpData)
+	if err != nil {
+		return 0, err
+	}
+	//correctly assign data len
+	p.mpData = p.mpData[:nr]
+
+	return int64(nr), nil
+}
+func (p *fieldPart) writeTo(chunk []byte, w io.Writer) error {
+	//chunk is not used.
+	return writeExactly(w, p.mpData)
+}
+func (p *fieldPart) close() error {
+	//do nothing
+	return nil
+}
+
+func (p *filePart) newPart(buf *bytes.Buffer, mpw *multipart.Writer) (int64, error) {
+	//make sure file is closed
+	if err := p.close(); err != nil {
+		return 0, err
+	}
+
+	//open file and get information
+	file, err := os.Open(p.filePath)
+	if err != nil {
+		return 0, err
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	p.file = file
+	p.fileSize = fi.Size()
+
+	//Create file part (the content is not writen)
+	if _, err := mpw.CreateFormFile(p.fieldName, p.baseName); err != nil {
+		return 0, err
+	}
+
+	n := buf.Len()
+	if cap(p.mpBegin) < n {
+		p.mpBegin = make([]byte, n)
+	}
+	nr, err := buf.Read(p.mpBegin)
+	if err != nil {
+		return int64(nr), err
+	}
+	//correctly assign data len
+	p.mpBegin = p.mpBegin[:nr]
+
+	return int64(n) + p.fileSize, nil
+}
+func (p *filePart) writeTo(chunk []byte, w io.Writer) error {
+	//write multipart begin
+	if err := writeExactly(w, p.mpBegin); err != nil {
+		return err
+	}
+
+	if nw, err := io.CopyBuffer(w, p.file, chunk); err != nil {
+		return err
+	} else if nw != p.fileSize {
+		//size doesn't match
+		return fmt.Errorf("file size (%v) != copy size (%v)", p.fileSize, nw)
+	}
+
+	return nil
+}
+func (p *filePart) close() error {
+	if f := p.file; f != nil {
+		p.file = nil
+		if err := f.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *endPart) newPart(buf *bytes.Buffer, mpw *multipart.Writer) (int64, error) {
+	//end boundary
+	if err := mpw.Close(); err != nil {
+		return 0, err
+	}
+
+	//Read writen data from buffer
+	n := buf.Len()
+	if cap(p.mpData) < n {
+		p.mpData = make([]byte, n)
+	}
+	nr, err := buf.Read(p.mpData)
+	if err != nil {
+		return 0, err
+	}
+	//correctly assign data len
+	p.mpData = p.mpData[:nr]
+
+	return int64(nr), nil
+
+}
+func (p *endPart) writeTo(chunk []byte, w io.Writer) error {
+	//chunk is not used.
+	return writeExactly(w, p.mpData)
+}
+func (p *endPart) close() error {
+	return nil
+}
+
+func (fu *formUploader) AddField(name, value string) FormUploader {
+	fp := &fieldPart{name: name, value: value}
+	fu.fields = append(fu.fields, fp)
+
+	return fu
+}
+func (fu *formUploader) AddFields(fields map[string]string) FormUploader {
+	for key, val := range fields {
+		fp := &fieldPart{name: key, value: val}
+		fu.fields = append(fu.fields, fp)
+	}
+	return fu
+}
+func (fu *formUploader) Fields(name string) []string {
+	fields := []string{}
+	for _, field := range fu.fields {
+		if field.name == name {
+			fields = append(fields, field.value)
+		}
+	}
+	return fields
+}
+func (fu *formUploader) AddFiles(fieldName string, filesPath ...string) error {
+	baseNames := make(map[string]bool)
+	for _, filePath := range filesPath {
+		filePath, err := filepath.Abs(filePath)
+		if err != nil {
+			return err
+		}
+		baseName := filepath.Base(filePath)
+		name := baseName
+		for n := 1; n < 1000; n++ {
+			//name doesn't exist (no name conflict)
+			if _, ok := baseNames[name]; !ok {
+				baseNames[name] = true
+				break
+			}
+			name = fmt.Sprintf("%03d_%s", n, baseName)
+		}
+
+		fp := &filePart{
+			fieldName: fieldName,
+			filePath:  filePath,
+			baseName:  name,
+		}
+		fu.files = append(fu.files, fp)
+	}
+	return nil
+}
+func (fu *formUploader) Files() []string {
+	filePaths := []string{}
+	for _, fp := range fu.files {
+		filePaths = append(filePaths, fp.filePath)
+	}
+	return filePaths
+}
+func (fu *formUploader) SetChunkSize(size int64) FormUploader {
+	fu.chunkSize = size
+	return fu
+}
+func (fu *formUploader) ChunkSize() int64 {
+	return fu.chunkSize
+}
+func (fu *formUploader) Post(targetURL string) (*http.Response, error) {
+	return fu.submit(targetURL, "POST")
+}
+func (fu *formUploader) Put(targetURL string) (*http.Response, error) {
+	return fu.submit(targetURL, "PUT")
+}
+func (fu *formUploader) submit(targetURL, method string) (*http.Response, error) {
+	buf := &bytes.Buffer{}
+	mpw := multipart.NewWriter(buf)
+
+	//List of form parts: fields, files, end
+	parts := []formPart{}
+	for _, p := range fu.fields {
+		parts = append(parts, p)
+	}
+	for _, p := range fu.files {
+		parts = append(parts, p)
+	}
+	parts = append(parts, &endPart{})
+
+	//create parts and calculate size
+	totalContentLen := int64(0)
+	for _, p := range parts {
+		n, err := p.newPart(buf, mpw)
+		if err != nil {
+			return nil, err
+		}
+		totalContentLen += n
+	}
+
+	//close form parts when done
+	defer func() {
+		for _, p := range parts {
+			p.close()
+		}
+	}()
+
+	//reader writer
+	reader, writer := io.Pipe()
+	defer reader.Close()
+
+	//Write parts content
+	var routineErr error
+	go func() {
+		defer writer.Close()
+
+		chunk := make([]byte, fu.chunkSize)
+		for _, p := range parts {
+			if err := p.writeTo(chunk, writer); err != nil {
+				routineErr = err
+				break
+			}
+		}
+	}()
+
+	//construct HTTP client Request with rd
+	req, err := http.NewRequest(method, targetURL, reader)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", mpw.FormDataContentType())
+	req.ContentLength = totalContentLen
+
+	//process request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else if routineErr != nil {
+		return nil, routineErr
+	}
+
+	return resp, nil
+}
---
+++ http/formuploader_test.go
@@ -0,0 +1,193 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+//DON'T forget to add PORT to firewall exception
+var (
+	fileField    = "files"
+	dataDir      = "testdata/"
+	uploadDir    = "testdata/upload/"
+	uploadTarget = "/upload"
+	serverPort   = ":8080"
+	serverHost   = "http://localhost"
+)
+
+//File upload handler for testing.
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	const maxMemory = 1024 * 1024
+	err := r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//Our multipart form
+	multi := r.MultipartForm
+	log.Printf("File upload request received")
+
+	//handles form data
+	log.Printf("Process custom field(s)...")
+	for field, value := range multi.Value {
+		fmt.Printf("  Field[`%s`]: %v\n", field, value)
+	}
+
+	//Get file header.
+	log.Printf("Process file(s)...")
+	files := multi.File[fileField]
+	for _, fh := range files {
+		fmt.Printf("  %s\n", fh.Filename)
+		//Handle each files
+		file, err := fh.Open()
+		defer file.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		//Create destination file
+		dstFile, err := os.Create(uploadDir + fh.Filename)
+		defer dstFile.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		//Copy and overwrite destination file
+		if _, err := io.Copy(dstFile, file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	str := fmt.Sprintf("fields=%02d, files=%02d\n", len(multi.Value), len(multi.File))
+	w.Write([]byte(str))
+}
+
+func TestMain(m *testing.M) {
+	server := setup()
+	go server.ListenAndServe()
+
+	code := m.Run()
+	shutdown(server)
+	os.Exit(code)
+}
+
+func setup() *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc(uploadTarget, uploadHandler)
+
+	//DON'T forget to allow connection to PORT
+	//when the PC has firewall.
+	server := &http.Server{
+		Addr:    serverPort,
+		Handler: mux,
+	}
+
+	return server
+}
+
+func shutdown(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Error shutdown HTTP server: %v", err)
+	}
+	log.Printf("--- DONE ---")
+}
+
+func doUpload(fu FormUploader, failOnError bool, t *testing.T) {
+	url := serverHost + serverPort + uploadTarget
+	resp, err := fu.Post(url)
+	if err != nil {
+		if failOnError {
+			t.Fatal(err)
+		} else {
+			t.Logf("Error: %s", err)
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		if failOnError {
+			t.Fatal(err)
+		} else {
+			t.Logf("Error: %s", err)
+		}
+	}
+
+	log.Printf("Response=%v", string(response))
+}
+
+func TestFieldsOnly(t *testing.T) {
+	fu := NewFormUploader()
+	fu.AddField("id", "Field-only-upload")
+	fu.AddField("id", "Second ID")
+	fu.AddField("time", time.Now().Format(time.RFC3339))
+	fu.AddField("description", "Custom information")
+
+	//perform upload
+	doUpload(fu, true, t)
+}
+
+func TestSingleFileOnly(t *testing.T) {
+	fu := NewFormUploader()
+	fu.AddFiles(fileField, dataDir+"image01.jpg")
+
+	//perform upload
+	doUpload(fu, true, t)
+}
+
+func TestMultipleFilesOnly(t *testing.T) {
+	fu := NewFormUploader()
+	files := []string{
+		dataDir + "image01.jpg",
+		dataDir + "file01.txt",
+		dataDir + "file02.pdf",
+		dataDir + "file03.pdf",
+	}
+	fu.AddFiles(fileField, files...)
+
+	//perform upload
+	doUpload(fu, true, t)
+}
+
+func TestFilesWithFields(t *testing.T) {
+	fu := NewFormUploader()
+
+	//Add fields
+	fu.AddField("id", "File and custom files")
+	fu.AddField("time", time.Now().Format(time.RFC3339))
+	fu.AddField("description", "Custom information")
+
+	files := []string{
+		dataDir + "image01.jpg",
+		dataDir + "file01.txt",
+		dataDir + "file02.pdf",
+	}
+	fu.AddFiles(fileField, files...)
+
+	//perform upload
+	doUpload(fu, true, t)
+}
+
+func TestFileDoesNotExist(t *testing.T) {
+	fu := NewFormUploader()
+	files := []string{
+		dataDir + "file01.txt",
+		"desnotexist.txt",
+	}
+	fu.AddFiles(fileField, files...)
+
+	//perform upload
+	doUpload(fu, false, t)
+}
